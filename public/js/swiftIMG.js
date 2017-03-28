b34dd65a1697719afaa411ddc8a39448c1cf18cd
Vitalij Mudrij <vmudrij0508@gmail.com>
2017-03-28 19:12:34 +0300

    change swiftIMG and docx

--- public/js/swiftIMG.js
+++ public/js/swiftIMG.js
@@ -1,30 +1,81 @@
 class swiftImg {
     constructor(img) {
         this.img = img;
+        this.paramsArr = [];
     }
 
     rotate(degree = 90) {
-        this.sendToPort("rotate", degree);
+        let obj = {
+            "method" : "rotate",
+            "params" : {
+                "degree" : degree
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     crop(width = null, height = null, startX = null, startY = null,  isProp = false) {
-        this.sendToPort("rotate", width, height, startX, startY, isProp);
+        let obj = {
+            "method" : "rotate",
+            "params" : {
+                "width" : width,
+                "height" : height,
+                "startX" : startX,
+                "startY" : startY,
+                "isProp" : isProp
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    frame(color, width, isRounded = false) {
-
+    frame(type = 'type-1') {
+        let obj = {
+            "method" : "frame",
+            "params" : {
+                "type" : type
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    insert(image, position, offset, offsetY) {
-
+    insert(image, position = 'top-left', offsetX = 0, offsetY = 0) {
+        let obj = {
+            "method" : "insert",
+            "params" : {
+                "image" : image,
+                "position" : position,
+                "offsetX" : offsetX,
+                "offsetY" : offsetY
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    insertMerge(image, opacityValue, pos = 'top-left', offsetX = 0, offsetY = 0) {
-
+    insertMerge(image, opacityValue = 100, position = 'top-left', offsetX = 0, offsetY = 0) {
+        let obj = {
+            "method" : "insertMerge",
+            "params" : {
+                "image" : image,
+                "opacityValue" : opacityValue,
+                "position" : position,
+                "offsetX" : offsetX,
+                "offsetY" : offsetY
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    insertGrayscale(image, pos = 'top-left', $offsetX = 0, $offsetY = 0) {
-
+    insertGrayscale(image, position = 'top-left', offsetX = 0, offsetY = 0) {
+        let obj = {
+            "method" : "insertGrayscale",
+            "params" : {
+                "image" : image,
+                "position" : position,
+                "offsetX" : offsetX,
+                "offsetY" : offsetY
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     // insertResize(img, array size, $pos = 'top-left', $offsetX = 0, $offsetY = 0) {
@@ -36,96 +87,186 @@ class swiftImg {
     // }
 
     text(text, startX = 0, startY = 0) {
-
+        let obj = {
+            "method" : "text",
+            "params" : {
+                "text" : text,
+                "startX" : startX,
+                "startY" : startY
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    pixelate(size = 1) {
-
+    pixelate(size = 1)  {
+        let obj = {
+            "method" : "pixelate",
+            "params" : {
+                "size" : size
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    gamma(value = 40) {
-
+    gamma(value = 2) {
+        let obj = {
+            "method" : "gamma",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    bright(value = 40) {
-
+    bright(value = 15)  {
+        let obj = {
+            "method" : "bright",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    contrast(value = 50) {
-
+    contrast(value = 20) {
+        let obj = {
+            "method" : "bright",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    colorize(red = 40, green = 40, blue = 40) {
-
+    colorize(red = 10, green = 10, blue = 10) {
+        let obj = {
+            "method" : "bright",
+            "params" : {
+                "red" : red,
+                "green" : green,
+                "blue" : blue
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     makeGrayscale() {
-
+        let obj = {
+            "method" : "bright",
+            "params" : null
+        }
+        this.paramsArr.push(obj);
     }
 
     invert(filter) {
-
+        let obj = {
+            "method" : "invert",
+            "params" : {
+                "filter" : filter
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     mirror(value = 'h') {
-
+        let obj = {
+            "method" : "mirror",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
-
-    //Оптимизация. Тоже самое что и шестой пункт.
 
     opacity(value = 60) {
-
+        let obj = {
+            "method" : "opacity",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    makeGIF(imgArr, param1, param2) {
+    // makeGIF(imgArr, param1, param2) {
 
-    }
+    // }
 
     resize(width, heigh = null) {
-
+        let obj = {
+            "method" : "resize",
+            "params" : {
+                "width" : width,
+                "heigh" : heigh
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    borders(type = 'color', format = 'jpg', quality = 90, sigma = 1, sobelK = 1, low = 50, high = 150, size = 2) {
+    // borders(type = 'color', format = 'jpg', quality = 90, sigma = 1, sobelK = 1, low = 50, high = 150, size = 2) {
 
-    }
+    // }
 
     histogramEqualization(type = 'grayscale') {
-
+        let obj = {
+            "method" : "histogramEqualization",
+            "params" : {
+                "type" : type
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     histogramGraph(type = 'grayscale', format = 'jpg', quality = 90, coef = 35) {
-
+        let obj = {
+            "method" : "histogramGraph",
+            "params" : {
+                "type" : type,
+                "format" : format,
+                "quality" : quality,
+                "coef" : coef
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     blur(value = 1) {
-
+        let obj = {
+            "method" : "blur",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
-    filters(filters) {
+    // filters(filters) {
 
-    }
+    // }
 
     sharpen(value = 10) {
-
+        let obj = {
+            "method" : "sharpen",
+            "params" : {
+                "value" : value
+            }
+        }
+        this.paramsArr.push(obj);
     }
 
     outPut() {
-
-    }
-
-    save() {
-
-    }
-
-    sendToPort(method, ...params) {
         $.ajax({
             url: '/public/php/port.php',
             type: 'POST',
-            data: {method, params : JSON.stringify(params)},
+            data: {"paramsArr" : this.paramsArr},
             success: function(data) {
                 //return image
                 //this.img= data.img
             }
-        })
+        });
+    }
+
+    save() {
+
     }
 }
