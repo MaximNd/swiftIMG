24d7402ea69b870d2a250d82ba378bd10b362467
Max <max0350@gmail.com>
2017-04-24 11:56:24 +0300

    Heroku speedtest histogram

--- app/API.php
+++ app/API.php
@@ -25,7 +25,7 @@ $type = pathinfo($path, PATHINFO_EXTENSION);
 $data = file_get_contents($path);
 $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
- $img = new \app\swiftIMG($base64, 'jpg' , 100);
+//$img = new \app\swiftIMG($base64, 'jpg' , 100);
 
  //var_dump($img);
 //echo $base64;
@@ -41,7 +41,7 @@ $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
 // $img6 = new \app\swiftIMG('E:/OpenServer/domains/localhost/swiftIMG/images/LenaDark.png', 'jpg', 100);
 
-// $img7 = new \app\swiftIMG('E:/OpenServer/domains/localhost/swiftIMG/images/lena.jpg', 'jpg', 100);
+$img7 = new \app\swiftIMG('E:/OpenServer/domains/localhost/swiftIMG/images/lena.jpg', 'jpg', 100);
 
 // $img8 = new \app\swiftIMG('E:/OpenServer/domains/localhost/swiftIMG/images/test4.jpg', 'jpg', 100);
 
@@ -86,7 +86,7 @@ $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 //var_dump($img->getImageData());
 
 
-  echo $img->makeGrayscale()->outPut();
+  echo $img7->histogramEqualization('color')->outPut();
 
 
 ?>
