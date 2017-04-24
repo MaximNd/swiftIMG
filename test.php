a37624cf52c58df859c9c1279e66a3fe10401b3b
Max <max0350@gmail.com>
2017-04-24 18:22:24 +0300

    Heroku host 5

--- test.php
+++ test.php
@@ -6,7 +6,15 @@
 </head>
 <body>
 	<?php 
-            echo $_SERVER['HTTP_HOST'];
+            $url = "https://swiftimg.herokuapp.com";
+
+			// Validate url
+			if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+			    echo("$url is a valid URL");
+			} else {
+			    echo("$url is not a valid URL");
+			}
+			?>
         ?>
         <script>
         	console.log(location.protocol + "//" + location.hostname);
